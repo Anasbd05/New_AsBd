d76f46e5db1846d3aebd6fbe1ccbacb5b8090d96
anas bd <anastrying05@gmail.com>
2025-04-26 18:30:55 +0100

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='py-6 px-10 flex items-center border-b border-foreground justify-between'>
+            <h1 className='font-bold text-4xl text-gray-200 font-header'>AS_BD</h1>
+            <div className="flex gap-6 items-center">
+                <Link className='hover:text-primary' href={"#"}>Home</Link>
+                <Link className='hover:text-primary' href={"#about"}>About</Link>
+                <Link className='hover:text-primary' href={"#services"}>Services</Link>
+                <Link className='hover:text-primary' href={"#pricing"}>Pricing</Link>
+            </div>
+            <button className=' py-2 px-7 ring cursor-pointer ring-foreground hover:bg-foreground duration-500 rounded-lg'>Contact Me</button>
+        </nav>
+    )
+}
+
+export default Navbar
